82eb0295836306308bdece14d65523a672db0056
Ahmad Fatih Muzaqi <145949071+FatihMuzaqi@users.noreply.github.com>
2023-11-29 21:59:20 +0700

    Add files via upload

---
+++ Latihan54_Koordinat.java
@@ -0,0 +1,57 @@
+
+package oop;
+class Koordinat {
+    protected int x;
+    protected int y;
+
+    public Koordinat(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+class WarnaKoordinat extends Koordinat {
+
+    public String namaWarna;
+
+    public WarnaKoordinat(int x, int y, String namaWarna) {
+        super(x, y);
+        this.x = x;
+        this.y = y;
+        this.namaWarna = namaWarna;
+    }
+
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+}
+
+public class Latihan54_Koordinat {
+    
+    public static void main(String[] args) {
+        WarnaKoordinat  wk = new WarnaKoordinat(10, 4, "Jingga");
+        System.out.println("Warna Koordinat : " + wk.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : " + wk.getX() + ", y : " + wk.getY());
+    }
+}
+
